577024abe1aa25ce6711ac076a562717f2412f25
steve <steve@stevedeMacBook-Pro.local>
2018-07-01 21:28:17 +0800

    KeyBindings for mac os

--- startup.sh
+++ startup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
  
 hidutil property --set '{"UserKeyMapping":[{"HIDKeyboardModifierMappingSrc":0x700000039,"HIDKeyboardModifierMappingDst":0x7000000E0},{"HIDKeyboardModifierMappingSrc":0x7000000E0,"HIDKeyboardModifierMappingDst":0x700000039}]}'
+
+mkdir $HOME/Library/KeyBindings
+ln -i KeyBindings/DefaultKeyBinding.dict  ~/Library/KeyBindings/DefaultKeyBinding.dict
